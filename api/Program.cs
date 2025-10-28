ed2418385c14c37d21a963109ca235247fde39a8
Julian Bollig <julian.bollig@tngtech.com>
2025-10-28 16:21:58 +0100

    butler

--- api/Program.cs
+++ api/Program.cs
@@ -20,7 +20,7 @@ var newSummaries = new[]
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/sensitive", (string? password) =>
